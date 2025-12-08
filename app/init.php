a3a2a0bb01d28bb2693d054f8632f334302c2d17
ayomideRussianCode <ayindeayomide7@gmail.com>
2025-12-08 14:00:03 +0100

    initialized app file and created app dirs

---
+++ app/init.php
@@ -0,0 +1,22 @@
+<?php
+session_start();
+require_once __DIR__ . "/../config/config.php";
+require_once __DIR__ . "/../config/database.php";
+
+spl_autoload_register(function($class_name) {
+    $paths = [
+        __DIR__ . '/app/controllers' ,
+        __DIR__ . '/app/models',
+
+    ];
+
+    foreach($paths as $path){
+      
+        $file = $path . $class_name . 'php';
+
+        if(file_exists($file)) {
+            require_once $file;
+            return true;
+        }
+    }
+});
