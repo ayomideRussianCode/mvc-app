7923b6b265bf2cbfec36af9724c053c875ee2b5d
ayomideRussianCode <ayindeayomide7@gmail.com>
2025-12-08 17:27:01 +0100

    local domain setup

--- app/init.php
+++ app/init.php
@@ -1,12 +1,13 @@
 <?php
+
 session_start();
 require_once __DIR__ . "/../config/config.php";
 require_once __DIR__ . "/../config/database.php";
 
 spl_autoload_register(function($class_name) {
     $paths = [
-        __DIR__ . '/app/controllers' ,
-        __DIR__ . '/app/models',
+        __DIR__ . "/app/controllers" ,
+        __DIR__ . "/app/models",
 
     ];
 
@@ -16,7 +17,7 @@ spl_autoload_register(function($class_name) {
 
         if(file_exists($file)) {
             require_once $file;
-            return true;
+            return;
         }
     }
 });
