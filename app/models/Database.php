d803e6586462f6c9a0cf8be8c9a5d4d2bfe8ef4c
ayomideRussianCode <ayindeayomide7@gmail.com>
2025-12-15 13:48:40 +0100

    models - Database.php

---
+++ app/models/Database.php
@@ -0,0 +1,36 @@
+<?php
+
+class Database {
+
+
+    private static $instance = null;
+
+    private $connection;
+
+    public function __construct()
+    {
+        $config = require base_path('config/config.php');
+
+        $dbConfig = $config['database'];
+
+        $host = $dbConfig['host'];
+        $dbname = $dbConfig['database'];
+        $username = $dbConfig['username'];
+        $password = $dbConfig['password'];
+        $port = $dbConfig['port'];
+        $charset = $dbConfig['charset'];
+
+        $dsn = "mysql:host={$host};dbname={$dbname}; port={$port}; charset={$charset};";
+
+        try {
+
+            $this->connection = new PDO($dsn, $username, $password);
+            $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        } catch(PDOException $e) {
+
+            die("Database failed: " . $e->getMessage());
+        }
+
+    }
+}
--- config/database.php
+++ config/database.php
@@ -1,2 +1,11 @@
 <?php
 
+return[
+        'host' => 'localhost',
+        'database' => 'mvc_app_db',
+        'username' => 'root',
+        'password' => '',
+        'port' => 3306,
+        'charset' => 'utf8mb4',
+  
+];
